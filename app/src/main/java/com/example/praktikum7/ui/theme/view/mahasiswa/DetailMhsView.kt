7e48ae7088bb71d65b6bcc6c8c1e7c90b2ca6e43
DeniArif <deniarifwibowo82@gmail.com>
2024-12-16 20:59:27 +0700

    Melanjutkan kode  BodyDetailMhs

--- app/src/main/java/com/example/praktikum7/ui/theme/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/view/mahasiswa/DetailMhsView.kt
@@ -1,22 +1,36 @@
 package com.example.praktikum7.ui.theme.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum7.ui.theme.costumwidget.TopAppBar
 import com.example.praktikum7.ui.theme.viewmodel.DetailMhsViewModel
 import com.example.praktikum7.ui.theme.viewmodel.DetailUiState
 import com.example.praktikum7.ui.theme.viewmodel.PenyediaViewModel
+import com.example.praktikum7.ui.theme.viewmodel.toMahasiswaEntity
 
 @Composable
 fun DetailMhsView(
@@ -69,7 +83,61 @@ fun BodyDetailMhs (
     detailUiState: DetailUiState = DetailUiState(),
     onDeleteClick: () -> Unit = { }
 ){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() // Tampil loading
+            }
+        }
 
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog (
+                        onDeleteConfrim = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteClick = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
 }
 
 
