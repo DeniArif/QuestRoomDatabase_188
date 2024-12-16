010b452d1b8134ef3e15a1036b7b3d5f9a4f0571
DeniArif <deniarifwibowo82@gmail.com>
2024-12-16 19:26:47 +0700

    Menambahkan kode HomeMhsView.kt melanjutkan dan menambahkan Composable fun BodyHomeMhsView

--- app/src/main/java/com/example/praktikum7/ui/theme/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/view/mahasiswa/HomeMhsView.kt
@@ -1,17 +1,24 @@
 package com.example.praktikum7.ui.theme.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum7.ui.theme.costumwidget.TopAppBar
+import com.example.praktikum7.ui.theme.viewmodel.HomeUiState
 import com.example.praktikum7.ui.theme.viewmodel.MahasiswaViewModel
 import com.example.praktikum7.ui.theme.viewmodel.PenyediaViewModel
 
@@ -51,6 +58,29 @@ fun HomeMhsView(
             onClick = {
                 onDetailClick(it)
             },
+            modifier = Modifier.padding(innerPadding)
         )
     }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember{ SnackbarHostState()} //Snackbar state
+    when {
+        homeUiState.isLoading-> {
+            //menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+    }
+
 }
