a6f1c7612ae4851920ec54211bb859b0d9fce5b7
DeniArif <deniarifwibowo82@gmail.com>
2024-12-16 23:44:27 +0700

    Perbaikan eror dan menambahkan kode di PengelolaHalaman.kt dan UpdateMhsView

--- app/src/main/java/com/example/praktikum7/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum7/ui/navigation/PengelolaHalaman.kt
@@ -4,11 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.praktikum7.ui.view.mahasiswa.DestinasiInsert
+import com.example.praktikum7.ui.view.mahasiswa.DetailMhsView
 import com.example.praktikum7.ui.view.mahasiswa.InsertMhsView
+import com.example.praktikum7.ui.view.mahasiswa.UpdateMhsView
 
 @Composable
 fun PengelolaHalaman(
@@ -30,7 +34,51 @@ fun PengelolaHalaman(
                 onNavigate = {
                     // Aksi untuk navigasi ke halaman lain
                     navController.navigate("destinasiLain") // Ganti dengan rute yang sesuai
+                },
+                modifier = modifier,
+            )
+        }
+        composable(
+            DestinasiDetail.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM) {
+                    type = NavType.StringType
                 }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+
+            nim?.let { nim ->
+                DetailMhsView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${DestinasiUpdate.route}/$it")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+        composable(
+            DestinasiUpdate.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiUpdate.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            UpdateMhsView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
             )
         }
     }
--- app/src/main/java/com/example/praktikum7/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/mahasiswa/UpdateMhsView.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -16,13 +18,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum7.ui.viewmodel.PenyediaViewModel
 import com.example.praktikum7.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.example.praktikum7.ui.costumwidget.TopAppBar
 
 @Composable
 fun UpdateMhsView(
     onBack: () -> Unit,
     onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
     viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory)
-            Inisialisasi ViewModel
 ) {
     val uiState = viewModel.updateUiState //Ambil Ui state dari ViewModel
     val snackbarHostState = remember { SnackbarHostState() }
@@ -30,24 +37,57 @@ fun UpdateMhsView(
 
     //Observasi perubahan snackbarMessage
     LaunchedEffect(uiState.snackBarMessage) {
-        println("LaunchedEffect trigged")
+        println("LaunchedEffect triggered")
         uiState.snackBarMessage?.let { message ->
             println("Snackbar message received: $message")
-            snackbarHostState.showSnackbar(
+            coroutineScope.launch {
+                println("Launching coroutine for snackbar")
                 snackbarHostState.showSnackbar(
                     message = message,
                     duration = SnackbarDuration.Long
                 )
-            )
+                viewModel.resetSnackBarMessage()
+            }
         }
     }
 
     Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },//tempatkan Snackbar di Scaffold
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        }
+    ) { padding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
                 .padding(16.dp)
-        )
-    ) {  }
+        ) {
+
+            //Isi Body
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent) // Update state di ViewModel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()){
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate() // Navigasi di main thread
+                            }
+                        }
+
+                    }
+                }
+            )
+        }
+    }
 }
