8a4764b346fa93e1dcb488cfe52fef76ba3a50e0
DeniArif <deniarifwibowo82@gmail.com>
2024-12-16 19:18:51 +0700

    Menambahkan kode HomeMhsView.kt

---
+++ app/src/main/java/com/example/praktikum7/ui/theme/view/mahasiswa/HomeMhsView.kt
@@ -0,0 +1,56 @@
+package com.example.praktikum7.ui.theme.view.mahasiswa
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum7.ui.theme.costumwidget.TopAppBar
+import com.example.praktikum7.ui.theme.viewmodel.MahasiswaViewModel
+import com.example.praktikum7.ui.theme.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeMhsView(
+    viewModel: MahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa",
+                )
+            }
+        }
+    ){ innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+        )
+    }
+}
